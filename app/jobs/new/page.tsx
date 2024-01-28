8a8bb8345877cd5a12f85336a94a5443c797c364
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-01-28 16:12:28 +0530

    final fix

--- app/jobs/new/page.tsx
+++ app/jobs/new/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import React from "react";
 import JobForm from "./_components/JobForm";
 
-export const metaData: Metadata = {
+export const metadata: Metadata = {
   title: "Post a New Job",
 };
 
