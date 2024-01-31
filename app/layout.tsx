a01471d45dccb1047dd1fc58b77c59b2982348df
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-01-31 21:00:56 +0530

    final logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  icons: [{ url: "/logo.png", href: "/logo.png" }],
+  icons: [{ url: "/logo.jpg", href: "/logo.jpg" }],
   title: {
     default: "Jobs",
     template: "%s| Flow Jobs ",
